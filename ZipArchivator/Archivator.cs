86b7c02441ff9b15fc3a4bee3d55445028ff80e4
gelemani <fedorkrutikov7352@gmail.com>
2024-09-09 01:49:12 +0300

    kcfgv

--- ZipArchivator/Archivator.cs
+++ ZipArchivator/Archivator.cs
@@ -1,95 +1,158 @@
 using System.IO.Compression;
 using System.IO;
 using System;
+using System.Collections.Generic;
 using System.Data;
 
-namespace ZipArchivator;
-
-class Archivator
+namespace ZipArchivator
 {
-    private const string zipPath = "blablabla.7z";
-    public const string lasertagMusicPath = "Hydrogen.mp3";
-
-    public void Archive(string fileName)
+    class Archivator
     {
-        // существует ли файл (валидация)
-        // проверка не архив ли
-
-        // заархивировать содержимое файла
-        // сохранить
-
-        Validate(fileName);
-
-        //using FileStream originalStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-        //using FileStream outputStream = new FileStream(zipPath, FileMode.Create, FileAccess.Write);
-        //using GZipStream gZipStream = new GZipStream(outputStream, CompressionMode.Compress);
-        //originalStream.CopyTo(gZipStream);
-
-        var buffer = new byte[1024 * 1024];
-        using (FileStream originalStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+        private const string zipPath = "blablabla.7z";
+        public const string lasertagMusicPath = "Hydrogen.mp3";
+        private const int _bufferSize = 1024 * 1024;
+        public void Archive(string fileName)
         {
-            using (FileStream outputStream = new FileStream(zipPath, FileMode.Create, FileAccess.Write))
+            // существует ли файл (валидация)
+            // проверка не архив ли
+
+            // заархивировать содержимое файла
+            // сохранить
+
+            Validate(fileName);
+
+            //using FileStream originalStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            //using FileStream outputStream = new FileStream(zipPath, FileMode.Create, FileAccess.Write);
+            //using GZipStream gZipStream = new GZipStream(outputStream, CompressionMode.Compress);
+            //originalStream.CopyTo(gZipStream);
+
+            var fileSize = new FileInfo(fileName).Length;
+
+            var buffer = new byte[_bufferSize];
+
+            using (FileStream originalStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
             {
-                int bytesRead;
-                while ((bytesRead = originalStream.Read(buffer, 0, buffer.Length)) > 0)
+                using (FileStream outputStream = new FileStream(zipPath, FileMode.Create, FileAccess.Write))
                 {
-                    using (MemoryStream ms = new MemoryStream()) 
+                    int bytesRead;
+
+                    // ???? ???????? ?????? ??????, ???-?? ????????,
+                    //  fileSize / _bufferSize = 6 (iterations)
+                    // fileSize % _bufferSize = 500 kB 
+
+                    int iterations = (int)(fileSize / _bufferSize);
+                    int remainder = (int)(fileSize % _bufferSize);
+
+                    Queue<byte[]> blocksRead = new(iterations + 1);
+
+                    for (int i = 0; i < iterations; i++)
                     {
-                        using (GZipStream gZipStream = new GZipStream(ms, CompressionMode.Compress))
-                        { 
-                            gZipStream.Write(buffer, 0, buffer.Length);
-                        }
-                        var compressedBuffer = ms.ToArray();
-                        outputStream.Write(compressedBuffer, 0, compressedBuffer.Length);
+                        bytesRead = originalStream.Read(buffer, 0, buffer.Length);
+                        blocksRead.Enqueue((byte[])buffer.Clone());
                     }
-                }
-            }
-        }
-    }
 
-    public void Unarchive(string zipPath)
-    {
-        //using FileStream originalStream = new FileStream(zipPath, FileMode.Open, FileAccess.Read);
-        //using FileStream outputStream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
-        //using GZipStream gZipStream = new GZipStream(originalStream, CompressionMode.Decompress);
-        //gZipStream.CopyTo(outputStream);
+                    bytesRead = originalStream.Read(buffer, 0, remainder);
+                    blocksRead.Enqueue((byte[])buffer.Clone());
 
-        var buffer = new byte[1024 * 1024];
-        using (FileStream originalStream = new FileStream(zipPath, FileMode.Open, FileAccess.Read))
-        {
-            using (FileStream outputStream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
-            {
-                int bytesRead;
-                while ((bytesRead = originalStream.Read(buffer, 0, buffer.Length)) > 0)
-                {
-                    using (MemoryStream ms = new MemoryStream())
+                    while (blocksRead.Count() > 0)
                     {
-                        using (GZipStream gZipStream = new GZipStream(ms, CompressionMode.Compress))
+                        using (MemoryStream ms = new MemoryStream())
                         {
-                            gZipStream.Write(buffer, 0, buffer.Length);
+                            var currentBlock = blocksRead.Dequeue();
+                            using (GZipStream gZipStream = new GZipStream(ms, CompressionMode.Compress))
+                            {
+                                gZipStream.Write(currentBlock, 0, currentBlock.Length);
+                            }
+
+                            var compressedBuffer = ms.ToArray();
+                            outputStream.Write(compressedBuffer, 0, compressedBuffer.Length);
                         }
-                        var compressedBuffer = ms.ToArray();
-                        outputStream.Write(compressedBuffer, 0, compressedBuffer.Length);
                     }
                 }
             }
         }
 
+        public void Unarchive(string zipPath)
+        {
+            Validate(zipPath);
 
-    static void Validate(string fileName)
-    {
-        if (!File.Exists(fileName))
-        {
-            throw new FileNotFoundException();
+            //using FileStream originalStream = new FileStream(zipPath, FileMode.Open, FileAccess.Read);
+            //using FileStream outputStream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            //using GZipStream gZipStream = new GZipStream(originalStream, CompressionMode.Decompress);
+            //gZipStream.CopyTo(outputStream);
+
+            string fileName = lasertagMusicPath;
+            var fileSize = new FileInfo(zipPath).Length;
+            var buffer = new byte[_bufferSize];
+
+            using (FileStream originalStream = new FileStream(zipPath, FileMode.Open, FileAccess.Read))
+            {
+                using (FileStream outputStream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+                {
+                    int bytesRead;
+                    int iterations = (int)(fileSize / _bufferSize);
+                    int remainder = (int)(fileSize % _bufferSize);
+
+                    Queue<byte[]> blocksRead = new(iterations + 1);
+
+                    for (int i = 0; i < iterations; i++)
+                    {
+                        bytesRead = originalStream.Read(buffer, 0, buffer.Length);
+                        blocksRead.Enqueue((byte[])buffer.Clone());
+                    }
+
+                    bytesRead = originalStream.Read(buffer, 0, remainder);
+                    blocksRead.Enqueue((byte[])buffer.Clone());
+
+                    while (blocksRead.Count() > 0)
+                    {
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            var currentBlock = blocksRead.Dequeue();
+                            using (GZipStream gZipStream = new GZipStream(ms, CompressionMode.Compress))
+                            {
+                                gZipStream.Write(currentBlock, 0, currentBlock.Length);
+                            }
+
+                            var compressedBuffer = ms.ToArray();
+                            outputStream.Write(compressedBuffer, 0, compressedBuffer.Length);
+                        }
+                    }
+
+                    while ((bytesRead = originalStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            using (GZipStream gZipStream = new GZipStream(ms, CompressionMode.Compress))
+                            {
+                                gZipStream.Write(buffer, 0, buffer.Length);
+                            }
+
+                            var compressedBuffer = ms.ToArray();
+                            outputStream.Write(compressedBuffer, 0, compressedBuffer.Length);
+                        }
+                    }
+                }
+            }
         }
-        if (ifArchive(fileName))
+
+        static void Validate(string fileName)
         {
-            throw new Exception();
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException();
+            }
+
+            if (ifArchive(fileName))
+            {
+                throw new Exception();
+            }
+        }
+
+        static bool ifArchive(string fileName)
+        {
+            return false;
+
         }
     }
-
-    static bool ifArchive(string fileName)
-    {
-        return false;
-    }    
 }
